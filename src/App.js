d226c6598707d497b955d2dd690653def0667939
Tareq-dev <tareque.dev@gmail.com>
2022-12-28 23:54:35 +0600

    Night and light mode  done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
+import { Route, Routes } from "react-router-dom";
+import SideMenu from "./scenes/global/SideMenu";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,8 +11,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideMenu />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ src/scenes/global/SideMenu.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { tokens } from "../../theme";
+import { MenuOutlined } from "@mui/icons-material";
+
+function SideMenu() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <Sidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlined /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlined />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+}
+
+export default SideMenu;
