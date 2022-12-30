7b32acb1613e478c849e2e08448aaf7c337efa86
Tareq-dev <tareque.dev@gmail.com>
2022-12-30 12:09:08 +0600

    Team setup

--- src/scenes/Team/Team.js
+++ src/scenes/Team/Team.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityUpdateOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import React from "react";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "./../../data/mockData";
 
 function Team() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgoundColor={
+              access === "admin"
+                ? colors.greenAccent[600]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === "admin" && <AdminPanelSettingsOutlined />}
+            {access === "manager" && <SecurityUpdateOutlined />}
+            {access === "user" && <LockOpenOutlined />}
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
   return (
-    <div>
-    Team
-    </div>
-  )
+    <Box m='20px'>
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box m='40px 0 0 0' height='75vh'>
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 }
 
 export default Team;
