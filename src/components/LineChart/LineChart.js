05f1b69b4ce54266aa4a87fffcc0314b0c4f430b
Tareq-dev <tareque.dev@gmail.com>
2022-12-31 21:03:43 +0600

    Data analysis

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -1,12 +1,111 @@
+import React, { useEffect, useState } from "react";
 import { ResponsiveLine } from "@nivo/line";
-import React from "react";
 import { mockLineData } from "../../data/mockData";
 import { useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 
-function LineChart({ isDashboard = false }) {
+function LineChart({ isDashboard = false,barData }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [givenData, setGivenData] = useState();
+
+  
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const line = data.filter((sec) => sec.sector.length > 0);
+        const xLine = line.map((l) => l.sector);
+        const yLine = line.map((l) => l.intensity);
+
+        function splitArrayByDuplicates(arr) {
+          const result = {};
+          for (const elem of arr) {
+            if (!result[elem]) {
+              result[elem] = [elem];
+            } else {
+              result[elem].push(elem);
+            }
+          }
+          return Object.values(result);
+        }
+        const xdata = splitArrayByDuplicates(xLine);
+        const ydata = splitArrayByDuplicates(yLine);
+        const xx = xdata.map((x) => x[0]);
+        const yy = ydata.map((y) => y[0]);
+        const spliceYY = yy.slice(0, 18);
+        const axis = [
+          {
+            x: xx[0],
+            y: spliceYY[0],
+          },
+          {
+            x: xx[1],
+            y: spliceYY[1],
+          },
+          {
+            x: xx[2],
+            y: spliceYY[2],
+          },
+          {
+            x: xx[3],
+            y: spliceYY[3],
+          },
+          {
+            x: xx[4],
+            y: spliceYY[4],
+          },
+          {
+            x: xx[5],
+            y: spliceYY[5],
+          },
+          {
+            x: xx[6],
+            y: spliceYY[6],
+          },
+          {
+            x: xx[7],
+            y: spliceYY[7],
+          },
+          {
+            x: xx[8],
+            y: spliceYY[8],
+          },
+          {
+            x: xx[9],
+            y: spliceYY[9],
+          },
+          {
+            x: xx[10],
+            y: spliceYY[10],
+          },
+          {
+            x: xx[11],
+            y: spliceYY[12],
+          },
+        ];
+        const barData = [
+          {
+            id: "japan",
+            color: tokens("dark").greenAccent[500],
+            data: axis,
+          },
+          {
+            id: "koan",
+            color: tokens("dark").greenAccent[500],
+            data: axis,
+          },
+          {
+            id: "lajs",
+            color: tokens("dark").greenAccent[500],
+            data: axis,
+          },
+        ];
+
+        console.log(barData);
+        console.log(mockLineData);
+      });
+  }, []);
   return (
     <ResponsiveLine
       data={mockLineData}
@@ -68,7 +167,7 @@ function LineChart({ isDashboard = false }) {
       axisLeft={{
         orient: "left",
         tickSize: 5,
-        tickValues:5,
+        tickValues: 5,
         tickPadding: 5,
         tickRotation: 0,
         legend: isDashboard ? undefined : "count",
