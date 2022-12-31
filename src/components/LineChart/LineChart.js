a638d2f171fb30d054c7a18f9ee1723adfe41286
Tareq-dev <tareque.dev@gmail.com>
2022-12-31 21:41:26 +0600

    Data analysis linechart

--- src/components/LineChart/LineChart.js
+++ src/components/LineChart/LineChart.js
@@ -4,19 +4,24 @@ import { mockLineData } from "../../data/mockData";
 import { useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 
-function LineChart({ isDashboard = false,barData }) {
+function LineChart({ isDashboard = false, barData }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [givenData, setGivenData] = useState();
+  const [givenData, setGivenData] = useState([]);
 
-  
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
       .then((data) => {
-        const line = data.filter((sec) => sec.sector.length > 0);
-        const xLine = line.map((l) => l.sector);
-        const yLine = line.map((l) => l.intensity);
+        const usa = data.filter((c) => c.country === "United States of America");
+        const brazil = data.filter((c) => c.country === "Brazil");
+        const India = data.filter((c) => c.country === "India");
+        const xLineUsa = usa.map((l) => l.sector);
+        const yLineUsa = usa.map((l) => l.intensity);
+        const xLinebrazil = brazil.map((l) => l.sector);
+        const yLinebrazil = brazil.map((l) => l.intensity);
+        const xLineIndia = India.map((l) => l.sector);
+        const yLineIndia = India.map((l) => l.intensity);
 
         function splitArrayByDuplicates(arr) {
           const result = {};
@@ -29,86 +34,195 @@ function LineChart({ isDashboard = false,barData }) {
           }
           return Object.values(result);
         }
-        const xdata = splitArrayByDuplicates(xLine);
-        const ydata = splitArrayByDuplicates(yLine);
-        const xx = xdata.map((x) => x[0]);
-        const yy = ydata.map((y) => y[0]);
-        const spliceYY = yy.slice(0, 18);
-        const axis = [
+        const xdataUsa = splitArrayByDuplicates(xLineUsa);
+        const ydataUsa = splitArrayByDuplicates(yLineUsa);
+        const xdatabrazil = splitArrayByDuplicates(xLinebrazil);
+        const ydatabrazil = splitArrayByDuplicates(yLinebrazil);
+        const xdataIndia = splitArrayByDuplicates(xLineIndia);
+        const ydataIndia = splitArrayByDuplicates(yLineIndia);
+
+        const xUsa = xdataUsa.map((x) => x[0]);
+        const yUsa = ydataUsa.map((y) => y[0]);
+        const xBrazil = xdatabrazil.map((x) => x[0]);
+        const yBrazil = ydatabrazil.map((y) => y[0]);
+        const xIndia = xdataIndia.map((x) => x[0]);
+        const yIndia = ydataIndia.map((y) => y[0]);
+        console.log(yIndia)
+        console.log(xIndia)
+        const axisUsa = [
           {
-            x: xx[0],
-            y: spliceYY[0],
+            x: xUsa[0],
+            y: yUsa[0],
           },
           {
-            x: xx[1],
-            y: spliceYY[1],
+            x: xUsa[1],
+            y: yUsa[1],
           },
           {
-            x: xx[2],
-            y: spliceYY[2],
+            x: xUsa[2],
+            y: yUsa[2],
           },
           {
-            x: xx[3],
-            y: spliceYY[3],
+            x: xUsa[3],
+            y: yUsa[3],
           },
           {
-            x: xx[4],
-            y: spliceYY[4],
+            x: xUsa[4],
+            y: yUsa[4],
           },
           {
-            x: xx[5],
-            y: spliceYY[5],
+            x: xUsa[5],
+            y: yUsa[5],
           },
           {
-            x: xx[6],
-            y: spliceYY[6],
+            x: xUsa[6],
+            y: yUsa[6],
           },
           {
-            x: xx[7],
-            y: spliceYY[7],
+            x: xUsa[7],
+            y: yUsa[7],
           },
           {
-            x: xx[8],
-            y: spliceYY[8],
+            x: xUsa[8],
+            y: yUsa[8],
           },
           {
-            x: xx[9],
-            y: spliceYY[9],
+            x: xUsa[9],
+            y: yUsa[9],
           },
           {
-            x: xx[10],
-            y: spliceYY[10],
+            x: xUsa[10],
+            y: yUsa[10],
           },
           {
-            x: xx[11],
-            y: spliceYY[12],
+            x: xUsa[11],
+            y: yUsa[11],
+          },
+        ];
+        const axisBrazil = [
+          {
+            x: xBrazil[0],
+            y: yBrazil[0],
+          },
+          {
+            x: xBrazil[1],
+            y: yBrazil[1],
+          },
+          {
+            x: xBrazil[2],
+            y: yBrazil[2],
+          },
+          {
+            x: xBrazil[3],
+            y: yBrazil[3],
+          },
+          {
+            x: xBrazil[4],
+            y: yBrazil[4],
+          },
+          {
+            x: xBrazil[5],
+            y: yBrazil[5],
+          },
+          {
+            x: xBrazil[6],
+            y: yBrazil[6],
+          },
+          {
+            x: xBrazil[7],
+            y: yBrazil[7],
+          },
+          {
+            x: xBrazil[8],
+            y: yBrazil[8],
+          },
+          {
+            x: xBrazil[9],
+            y: yBrazil[9],
+          },
+          {
+            x: xBrazil[10],
+            y: yBrazil[10],
+          },
+          {
+            x: xBrazil[11],
+            y: yBrazil[11],
+          },
+        ];
+        const axisIndia = [
+          {
+            x: xIndia[0],
+            y: yIndia[0],
+          },
+          {
+            x: xIndia[1],
+            y: yIndia[1],
+          },
+          {
+            x: xIndia[2],
+            y: yIndia[2],
+          },
+          {
+            x: xIndia[3],
+            y: yIndia[3],
+          },
+          {
+            x: xIndia[4],
+            y: yIndia[4],
+          },
+          {
+            x: xIndia[5],
+            y: yIndia[5],
+          },
+          {
+            x: xIndia[6],
+            y: yIndia[6],
+          },
+          {
+            x: xIndia[7],
+            y: yIndia[7],
+          },
+          {
+            x: xIndia[8],
+            y: yIndia[8],
+          },
+          {
+            x: xIndia[9],
+            y: yIndia[9],
+          },
+          {
+            x: xIndia[10],
+            y: yIndia[10],
+          },
+          {
+            x: xIndia[11],
+            y: yIndia[11],
           },
         ];
         const barData = [
           {
-            id: "japan",
+            id: "America",
             color: tokens("dark").greenAccent[500],
-            data: axis,
+            data: axisUsa,
           },
           {
-            id: "koan",
-            color: tokens("dark").greenAccent[500],
-            data: axis,
+            id: "Brazil",
+            color: tokens("dark").greenAccent[100],
+            data: axisBrazil,
           },
           {
-            id: "lajs",
-            color: tokens("dark").greenAccent[500],
-            data: axis,
-          },
+            id: "India",
+            color: tokens("dark").primary[500],
+            data: axisIndia,
+          }
         ];
-
-        console.log(barData);
-        console.log(mockLineData);
+        setGivenData(barData);
       });
   }, []);
+  // console.log(givenData);
   return (
     <ResponsiveLine
-      data={mockLineData}
+      data={givenData}
       theme={{
         axis: {
           domain: {
